bf4aa5199843d512bf957002eb71b0221d33722a
JARAWA <129917912+JARAWA@users.noreply.github.com>
2025-05-15 12:04:02 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JosaaRankTrendAnalyzer from './components/JosaaRankTrendAnalyzer';
+
+function App() {
+  return (
+    <div className="App">
+      <JosaaRankTrendAnalyzer />
+    </div>
+  );
+}
+
+export default App;
